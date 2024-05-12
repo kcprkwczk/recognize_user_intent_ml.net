a3e1dda301db8ea8bb9b28b7bd8eb18a440d9cba
Kacper Kawczak <kacper.kawczak@iteo.com>
2024-05-12 12:50:26 +0200

    refactor
    Signed-off-by: Kacper Kawczak <kacper.kawczak@iteo.com>

--- obj/Debug/net7.0/ConsoleApp4.AssemblyInfo.cs
+++ obj/Debug/net7.0/ConsoleApp4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a92a418c964fd6d420f4b903347527dec55c71")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
